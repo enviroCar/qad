873eed90d9bac748ec6d88922bdae853fdea2416
Christian Autermann <c.autermann@52north.org>
2019-11-22 11:51:44 +0100

    refactorings

--- src/main/java/org/envirocar/qad/utils/TypeSafeSpatialIndex.java
+++ src/main/java/org/envirocar/qad/utils/TypeSafeSpatialIndex.java
@@ -8,15 +8,19 @@ import org.locationtech.jts.index.strtree.STRtree;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class TypeSafeSpatialIndex<T> {
     private final SpatialIndex index;
+    private Function<T, Envelope> envelopeFunction;
 
     public TypeSafeSpatialIndex(Collection<T> items, Function<T, Envelope> envelopeFunction) {
         index = new STRtree();
-        items.forEach(item -> index.insert(envelopeFunction.apply(item), item));
+        this.envelopeFunction = Objects.requireNonNull(envelopeFunction);
+        items.forEach(this::insert);
     }
 
     @SuppressWarnings("unchecked")
@@ -32,15 +36,30 @@ public class TypeSafeSpatialIndex<T> {
         return items;
     }
 
+    @SuppressWarnings("unchecked")
+    public void query(Envelope search, Consumer<T> consumer) {
+        Objects.requireNonNull(consumer);
+        index.query(search, item -> consumer.accept((T) item));
+    }
+
+    public void insert(T item) {
+        index.insert(envelopeFunction.apply(item), item);
+    }
+
     public List<T> query(Geometry search, Predicate<T> predicate) {
         return query(search.getEnvelopeInternal(), predicate);
     }
 
+    public void query(Geometry search, Consumer<T> consumer) {
+        query(search.getEnvelopeInternal(), consumer);
+    }
+
     public List<T> query(Geometry search) {
-        return query(search, null);
+        return query(search, (Predicate<T>) null);
     }
 
     public List<T> query(Envelope search) {
-        return query(search, null);
+        return query(search, (Predicate<T>) null);
     }
 }
+
