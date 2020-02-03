3746024549b5ec045ffe559a2da11caa4cb37998
Christian Autermann <c.autermann@52north.org>
2020-02-03 09:12:22 +0100

    added new Chemnits model

--- src/main/java/org/envirocar/qad/configuration/AxisModelConfiguration.java
+++ src/main/java/org/envirocar/qad/configuration/AxisModelConfiguration.java
@@ -36,7 +36,7 @@ public class AxisModelConfiguration {
 
     @Bean(name = CHEMNITZ)
     public AxisModel modelChemnitz(ObjectMapper mapper, AxisModelParser parser) throws IOException {
-        return readModel(mapper, parser, "/Chemnitz-2019-11-18.json");
+        return readModel(mapper, parser, "/Chemnitz-2020-01-28.json");
     }
 
     private AxisModel readModel(ObjectMapper mapper, AxisModelParser parser, String path) throws IOException {
