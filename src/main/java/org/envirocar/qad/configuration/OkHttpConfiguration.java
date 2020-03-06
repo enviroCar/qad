c37bd179f0389402026d87ca03299fb35c8019ff
Christian Autermann <c.autermann@52north.org>
2020-03-06 13:09:54 +0100

    clean up imports

--- src/main/java/org/envirocar/qad/configuration/OkHttpConfiguration.java
+++ src/main/java/org/envirocar/qad/configuration/OkHttpConfiguration.java
@@ -1,16 +1,8 @@
 package org.envirocar.qad.configuration;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import okhttp3.OkHttpClient;
-import org.envirocar.qad.AlgorithmParameters;
-import org.envirocar.qad.mapmatching.MapMatcher;
-import org.envirocar.qad.mapmatching.MapMatcherImpl;
-import org.envirocar.qad.mapmatching.MapMatchingService;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import retrofit2.Retrofit;
-import retrofit2.converter.jackson.JacksonConverterFactory;
 
 @Configuration
 public class OkHttpConfiguration {
